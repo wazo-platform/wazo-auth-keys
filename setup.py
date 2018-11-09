f52c77eb986f1a1a74c562f716d18db454af6aec
Francois Blackburn <fblackburn@wazo.community>
2018-11-09 15:05:06 -0500

    add an empty python project

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='wazo-auth-keys',
+    version='1.0',
+    author='Wazo Authors',
+    author_email='dev@wazo.community',
+    url='http://wazo.community',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'wazo-auth-keys = wazo_auth_keys.main:main',
+        ],
+    },
+)
---
+++ wazo_auth_keys/main.py
@@ -0,0 +1,12 @@
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import sys
+
+
+def main(argv=sys.argv[1:]):
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
