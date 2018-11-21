e7392ba78ccb34e44670cd4fd6b8db7b9a43179f
Francois Blackburn <fblackburn@wazo.community>
2018-11-21 10:23:35 -0500

    tests: initialize wazo-auth-cli
    same behavior as bootstrap setup and complete

--- integration_tests/suite/helpers/base.py
+++ integration_tests/suite/helpers/base.py
@@ -4,9 +4,34 @@
 import os
 
 from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
+from xivo_auth_client import Client as AuthClient
+
+ASSETS = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'assets'))
+KEY_FILENAME = os.path.join(ASSETS, 'init-auth-key')
 
 
 class BaseIntegrationTest(AssetLaunchingTestCase):
 
-    assets_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'assets'))
+    assets_root = ASSETS
     service = 'auth-keys'
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.complete_wazo_auth_bootstrap()
+
+    @classmethod
+    def complete_wazo_auth_bootstrap(cls):
+        with open(KEY_FILENAME, 'r') as f:
+            for line in f:
+                key = line.strip()
+                break
+
+        body = {
+            'key': key,
+            'username': 'wazo-auth-cli',
+            'password': 'secret',
+            'purpose': 'internal',
+        }
+        auth = AuthClient('localhost', cls.service_port(9497, 'auth'), verify_certificate=False)
+        auth.init.run(**body)
