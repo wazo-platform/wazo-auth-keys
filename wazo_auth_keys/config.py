406f658d936c65816e11dea68d259e54f5ed91c6
Francois Blackburn <fblackburn@wazo.community>
2019-02-13 09:00:12 -0500

    update SPDX-License-Identifier

--- wazo_auth_keys/config.py
+++ wazo_auth_keys/config.py
@@ -1,5 +1,5 @@
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo.chain_map import ChainMap
 from xivo.config_helper import parse_config_dir, read_config_file_hierarchy
